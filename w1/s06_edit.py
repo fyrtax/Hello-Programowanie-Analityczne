0c48e810bcc0a9dbbc0ec53ef1d66f1e72a3f489
fyrtax <33764198+fyrtax@users.noreply.github.com>
2026-03-23 19:59:02 +0100

    Add save function

--- w1/s06_edit.py
+++ w1/s06_edit.py
@@ -1,45 +1,44 @@
 from kivy.app import App
 from kivy.uix.filechooser import FileChooserListView
 
+
 class S06_NaszEdit(App):
     def build(self):
         return self.root
 
     def przycisk_open(self, nazwaed, oknoed):
-        print('Naciśnięto Open')
-        
+        print('naciśnięto open')
+
         self.oknoed = oknoed
         self.nazwaed = nazwaed
         fc = FileChooserListView(path='.')
         fc.bind(on_submit=self.load)
         self.root.add_widget(fc)
-        
-        # tinaz = self.root.ids['tiNazwa']
-        # print(tinaz.text)
-        
-        # with open(nazwa.text) as plik:
-        #     tekst = plik.read()
-        #     print(tekst)
-        
-        # print(self.root.ids)
-        # tint = self.root.ids['lbNaszTekst']
-        # tint.text = tekst
-        # lab.text = tekst
-    
+
     def load(self, *args):
-        print(args)
         fc, nazwa, *reszta = args
-        print(nazwa)
         self.nazwaed.text = nazwa[0]
         with open(nazwa[0]) as plik:
-             tekst = plik.read()
-             print(tekst)
+            tekst = plik.read()
+
         self.oknoed.text = tekst
         self.root.remove_widget(fc)
 
-    def zapisz(self, nazwa, tekst):
-        with open(nazwa.text, 'w') as plik:
-            plik.write(tekst.text)
+    def zapisz(self, nazwaed, oknoed):
+        self.nazwaed = nazwaed
+        self.oknoed = oknoed
+        fc = FileChooserListView(path='.')
+        fc.bind(on_submit=self.do_save)
+        self.root.add_widget(fc)
+
+    def do_save(self, *args):
+        fc, nazwa, *reszta = args
+        self.nazwaed.text = nazwa[0]
+        with open(nazwa[0], 'w') as plik:
+            plik.write(self.oknoed.text)
+
+        self.root.remove_widget(fc)
+
 
 app = S06_NaszEdit()
 app.run()
